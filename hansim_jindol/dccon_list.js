765e4203fa3bf65e5e17426a44f69b4ca1d04fe2
k123s456h <k123s456h@gmail.com>
2023-01-21 04:13:24 +0900

    feat: 추가된 목록 정리

--- hansim_jindol/dccon_list.js
+++ hansim_jindol/dccon_list.js
@@ -1,129 +1,238 @@
 dcConsData = [
 
-/*에펙 디시콘 리스트*/
+  /*에펙 디시콘 리스트*/
 
-{"name":"푸하하.png", "keywords":["푸하하"]},
-{"name":"싫은데요.png", "keywords":["싫은데요"]},
-{"name":"눈물라라.png", "keywords":["눈물라라"]},
-{"name":"눈물램.png", "keywords":["눈물램파트"]},
-{"name":"눈물레버.png", "keywords":["눈물레버"]},
-{"name":"눈물레이스.png", "keywords":["눈물레이스"]},
-{"name":"눈물로바.png", "keywords":["눈물로바"]},
-{"name":"눈물발.png", "keywords":["눈물발키리"]},
-{"name":"눈물방갈.png", "keywords":["눈물방갈"]},
-{"name":"눈물뱀.png", "keywords":["눈물미라지"]},
-{"name":"눈물블하.png", "keywords":["눈물블하"]},
-{"name":"눈물옥.png", "keywords":["눈물옥테인"]},
-{"name":"눈물왓.png", "keywords":["눈물왓슨"]},
-{"name":"눈물지붕.png", "keywords":["눈물지붕"]},
-{"name":"눈물코.png", "keywords":["눈물코스틱"]},
-{"name":"눈물태준.png", "keywords":["눈물태준"]},
-{"name":"눈물패파.png", "keywords":["눈물패파"]},
-{"name":"눈물퓨즈.png", "keywords":["눈물퓨즈"]},
-{"name":"눈물호라.png", "keywords":["눈물호라"]},
-{"name":"그렇군요.png", "keywords":["그렇군요"]},
-{"name":"무신.png", "keywords":["무신"]},
-{"name":"안아줘요2.png", "keywords":["안아줘요2"]},
-{"name":"패파감사.png", "keywords":["패파감사"]},
-{"name":"이상해요.png", "keywords":["이상해요"]},
-{"name":"윙맨금지.png", "keywords":["윙맨금지"]},
-
-
-/*제리 디시콘 리스트*/
-
-{"name":"제리감사.gif", "keywords":["제리감사"]},
-{"name":"제리기도.gif", "keywords":["제리기도"]},
-{"name":"제리기도2.gif", "keywords":["제리기도2"]},
-{"name":"제리꿀잠.gif", "keywords":["제리꿀잠"]},
-{"name":"제리꿀잠2.gif", "keywords":["제리꿀잠2"]},
-{"name":"제리놀람.gif", "keywords":["제리놀람"]},
-{"name":"제리놀람2.gif", "keywords":["제리놀람2"]},
-{"name":"제리놀람3.gif", "keywords":["제리놀람3"]},
-{"name":"제리놀림.gif", "keywords":["제리놀림"]},
-{"name":"제리뇌정지.gif", "keywords":["제리뇌정지"]},
-{"name":"제리동의.gif", "keywords":["제리동의"]},
-{"name":"제리메롱.gif", "keywords":["제리메롱"]},
-{"name":"제리메롱2.gif", "keywords":["제리메롱2"]},
-{"name":"제리바이.gif", "keywords":["제리바이"]},
-{"name":"제리하이.gif", "keywords":["제리하이","제리안녕","제리인사"]},
-{"name":"제리발광.gif", "keywords":["제리발광"]},
-{"name":"제리발광2.gif", "keywords":["제리발광2"]},
-{"name":"제리사망.gif", "keywords":["제리사망"]},
-{"name":"제리수줍.gif", "keywords":["제리수줍"]},
-{"name":"제리아니.gif", "keywords":["제리아니"]},
-{"name":"제리웃음.gif", "keywords":["제리웃음"]},
-{"name":"제리응애.gif", "keywords":["제리응애"]},
-{"name":"제리주먹.gif", "keywords":["제리주먹"]},
-{"name":"제리쳐맞.gif", "keywords":["제리쳐맞"]},
-{"name":"제리쳐맞2.gif", "keywords":["제리쳐맞2"]},
-{"name":"제리쳐묵.gif", "keywords":["제리쳐묵"]},
-{"name":"제리쳐묵2.gif", "keywords":["제리쳐묵2"]},
-{"name":"제리퉤.gif", "keywords":["제리퉤"]},
-{"name":"제리파밍.gif", "keywords":["제리파밍"]},
-{"name":"제리한숨.gif", "keywords":["제리한숨"]},
-{"name":"제리한심.gif", "keywords":["제리한심"]},
-{"name":"제리한심2.gif", "keywords":["제리한심2"]},
-{"name":"제리화남.gif", "keywords":["제리화남"]},
-
-/*칠레감탄 디시콘 리스트*/
-
-{"name":"칠레감탄.gif", "keywords":["칠레감탄"]},
-{"name":"칠레감탄2.gif", "keywords":["칠레감탄2"]},
-{"name":"칠레감탄3.gif", "keywords":["칠레감탄3"]},
-{"name":"칠레감탄4.gif", "keywords":["칠레감탄4"]},
-{"name":"칠레감탄5.gif", "keywords":["칠레감탄5"]},
-
-/*야숭이 디시콘 리스트*/
-
-{"name":"야숭갈.png", "keywords":["갈"]},
-{"name":"야숭감탄.png", "keywords":["야숭감탄"]},
-{"name":"야숭고민.png", "keywords":["야숭고민"]},
-{"name":"야숭고민2.gif", "keywords":["야숭고민2"]},
-{"name":"야숭고민3.png", "keywords":["야숭고민3"]},
-{"name":"야숭내놔.gif", "keywords":["야숭내놔"]},
-{"name":"야숭벅벅.png", "keywords":["야숭벅벅"]},
-{"name":"야숭분노.png", "keywords":["야숭분노"]},
-{"name":"야숭분노2.gif", "keywords":["야숭분노2"]},
-{"name":"야숭분노3.png", "keywords":["야숭분노3"]},
-{"name":"야숭야스.png", "keywords":["야숭야스"]},
-{"name":"야숭야스2.png", "keywords":["야숭야스2"]},
-{"name":"야숭야스각.png", "keywords":["야스각"]},
-{"name":"야숭야스각2.png", "keywords":["야스각2"]},
-{"name":"야숭야스각3.png", "keywords":["야스각3"]},
-{"name":"야숭위태.gif", "keywords":["야숭위태"]},
-{"name":"야숭ㅗ.gif", "keywords":["ㅗ"]},
-{"name":"문열어.gif", "keywords":["문열어"]},
-
-/*털박이 디시콘 리스트*/
-
-{"name":"진돌무신.png", "keywords":["진돌무신"]},
-{"name":"진돌역적.png", "keywords":["진돌역적"]},
-{"name":"진돌따봉2.png", "keywords":["진돌따봉2"]},
-{"name":"진돌군침.png", "keywords":["진돌군침"]},
-{"name":"진돌군침2.png", "keywords":["진돌군침2"]},
-{"name":"진돌윙맨.png", "keywords":["진돌윙맨"]},
-{"name":"팬미팅.png", "keywords":["팬미팅"]},
-{"name":"진돌눈물.png", "keywords":["진돌눈물"]},
-{"name":"진돌파누.png", "keywords":["진돌파누"]},
-{"name":"진돌한숨.png", "keywords":["진돌한숨"]},
-{"name":"진돌쉴라.png", "keywords":["진돌쉴라"]},
+  { "name": "푸하하.png", "keywords": ["푸하하"] },
+  { "name": "싫은데요.png", "keywords": ["싫은데요"] },
+  { "name": "눈물라라.png", "keywords": ["눈물라라"] },
+  { "name": "눈물램.png", "keywords": ["눈물램파트"] },
+  { "name": "눈물레버.png", "keywords": ["눈물레버"] },
+  { "name": "눈물레이스.png", "keywords": ["눈물레이스"] },
+  { "name": "눈물로바.png", "keywords": ["눈물로바"] },
+  { "name": "눈물발.png", "keywords": ["눈물발키리"] },
+  { "name": "눈물방갈.png", "keywords": ["눈물방갈"] },
+  { "name": "눈물뱀.png", "keywords": ["눈물미라지"] },
+  { "name": "눈물블하.png", "keywords": ["눈물블하"] },
+  { "name": "눈물옥.png", "keywords": ["눈물옥테인"] },
+  { "name": "눈물왓.png", "keywords": ["눈물왓슨"] },
+  { "name": "눈물지붕.png", "keywords": ["눈물지붕"] },
+  { "name": "눈물코.png", "keywords": ["눈물코스틱"] },
+  { "name": "눈물태준.png", "keywords": ["눈물태준"] },
+  { "name": "눈물패파.png", "keywords": ["눈물패파"] },
+  { "name": "눈물퓨즈.png", "keywords": ["눈물퓨즈"] },
+  { "name": "눈물호라.png", "keywords": ["눈물호라"] },
+  { "name": "그렇군요.png", "keywords": ["그렇군요"] },
+  { "name": "무신.png", "keywords": ["무신"] },
+  { "name": "안아줘요2.png", "keywords": ["안아줘요2"] },
+  { "name": "패파감사.png", "keywords": ["패파감사"] },
+  { "name": "이상해요.png", "keywords": ["이상해요"] },
+  { "name": "윙맨금지.png", "keywords": ["윙맨금지"] },
 
 
+  /*제리 디시콘 리스트*/
 
-/*진돌 파송콘 리스트*/
+  { "name": "제리감사.gif", "keywords": ["제리감사"] },
+  { "name": "제리기도.gif", "keywords": ["제리기도"] },
+  { "name": "제리기도2.gif", "keywords": ["제리기도2"] },
+  { "name": "제리꿀잠.gif", "keywords": ["제리꿀잠"] },
+  { "name": "제리꿀잠2.gif", "keywords": ["제리꿀잠2"] },
+  { "name": "제리놀람.gif", "keywords": ["제리놀람"] },
+  { "name": "제리놀람2.gif", "keywords": ["제리놀람2"] },
+  { "name": "제리놀람3.gif", "keywords": ["제리놀람3"] },
+  { "name": "제리놀림.gif", "keywords": ["제리놀림"] },
+  { "name": "제리뇌정지.gif", "keywords": ["제리뇌정지"] },
+  { "name": "제리동의.gif", "keywords": ["제리동의"] },
+  { "name": "제리메롱.gif", "keywords": ["제리메롱"] },
+  { "name": "제리메롱2.gif", "keywords": ["제리메롱2"] },
+  { "name": "제리바이.gif", "keywords": ["제리바이"] },
+  { "name": "제리하이.gif", "keywords": ["제리하이", "제리안녕", "제리인사"] },
+  { "name": "제리발광.gif", "keywords": ["제리발광"] },
+  { "name": "제리발광2.gif", "keywords": ["제리발광2"] },
+  { "name": "제리사망.gif", "keywords": ["제리사망"] },
+  { "name": "제리수줍.gif", "keywords": ["제리수줍"] },
+  { "name": "제리아니.gif", "keywords": ["제리아니"] },
+  { "name": "제리웃음.gif", "keywords": ["제리웃음"] },
+  { "name": "제리응애.gif", "keywords": ["제리응애"] },
+  { "name": "제리주먹.gif", "keywords": ["제리주먹"] },
+  { "name": "제리쳐맞.gif", "keywords": ["제리쳐맞"] },
+  { "name": "제리쳐맞2.gif", "keywords": ["제리쳐맞2"] },
+  { "name": "제리쳐묵.gif", "keywords": ["제리쳐묵"] },
+  { "name": "제리쳐묵2.gif", "keywords": ["제리쳐묵2"] },
+  { "name": "제리퉤.gif", "keywords": ["제리퉤"] },
+  { "name": "제리파밍.gif", "keywords": ["제리파밍"] },
+  { "name": "제리한숨.gif", "keywords": ["제리한숨"] },
+  { "name": "제리한심.gif", "keywords": ["제리한심"] },
+  { "name": "제리한심2.gif", "keywords": ["제리한심2"] },
+  { "name": "제리화남.gif", "keywords": ["제리화남"] },
 
-{"name":"진돌페페.png", "keywords":["진돌페페"]},
-{"name":"진돌갈.png", "keywords":["진돌갈"]},
-{"name":"진돌깡.png", "keywords":["진돌깡"]},
-{"name":"진돌따봉.png", "keywords":["진돌따봉"]},
-{"name":"진돌야스.png", "keywords":["진돌야스"]},
-{"name":"진돌살1자.png", "keywords":["진돌살1자"]},
-{"name":"진돌하트.png", "keywords":["진돌하트"]},
-{"name":"안아줘요.png", "keywords":["안아줘요"]},
+  /*칠레감탄 디시콘 리스트*/
+
+  { "name": "칠레감탄.gif", "keywords": ["칠레감탄"] },
+  { "name": "칠레감탄2.gif", "keywords": ["칠레감탄2"] },
+  { "name": "칠레감탄3.gif", "keywords": ["칠레감탄3"] },
+  { "name": "칠레감탄4.gif", "keywords": ["칠레감탄4"] },
+  { "name": "칠레감탄5.gif", "keywords": ["칠레감탄5"] },
+
+  /*야숭이 디시콘 리스트*/
+
+  { "name": "야숭갈.png", "keywords": ["갈"] },
+  { "name": "야숭감탄.png", "keywords": ["야숭감탄"] },
+  { "name": "야숭고민.png", "keywords": ["야숭고민"] },
+  { "name": "야숭고민2.gif", "keywords": ["야숭고민2"] },
+  { "name": "야숭고민3.png", "keywords": ["야숭고민3"] },
+  { "name": "야숭내놔.gif", "keywords": ["야숭내놔"] },
+  { "name": "야숭벅벅.png", "keywords": ["야숭벅벅"] },
+  { "name": "야숭분노.png", "keywords": ["야숭분노"] },
+  { "name": "야숭분노2.gif", "keywords": ["야숭분노2"] },
+  { "name": "야숭분노3.png", "keywords": ["야숭분노3"] },
+  { "name": "야숭야스.png", "keywords": ["야숭야스"] },
+  { "name": "야숭야스2.png", "keywords": ["야숭야스2"] },
+  { "name": "야숭야스각.png", "keywords": ["야스각"] },
+  { "name": "야숭야스각2.png", "keywords": ["야스각2"] },
+  { "name": "야숭야스각3.png", "keywords": ["야스각3"] },
+  { "name": "야숭위태.gif", "keywords": ["야숭위태"] },
+  { "name": "야숭ㅗ.gif", "keywords": ["ㅗ"] },
+  { "name": "문열어.gif", "keywords": ["문열어"] },
+
+  /*털박이 디시콘 리스트*/
+
+  { "name": "진돌무신.png", "keywords": ["진돌무신"] },
+  { "name": "진돌역적.png", "keywords": ["진돌역적"] },
+  { "name": "진돌따봉2.png", "keywords": ["진돌따봉2"] },
+  { "name": "진돌군침.png", "keywords": ["진돌군침"] },
+  { "name": "진돌군침2.png", "keywords": ["진돌군침2"] },
+  { "name": "진돌윙맨.png", "keywords": ["진돌윙맨"] },
+  { "name": "팬미팅.png", "keywords": ["팬미팅"] },
+  { "name": "진돌눈물.png", "keywords": ["진돌눈물"] },
+  { "name": "진돌파누.png", "keywords": ["진돌파누"] },
+  { "name": "진돌한숨.png", "keywords": ["진돌한숨"] },
+  { "name": "진돌쉴라.png", "keywords": ["진돌쉴라"] },
 
 
-/*분류전 디시콘 리스트*/
-{"name":"불편.png", "keywords":["불편"]},
-{"name":"편안.png", "keywords":["편안"]}
+
+  /*진돌 파송콘 리스트*/
+
+  { "name": "진돌페페.png", "keywords": ["진돌페페"] },
+  { "name": "진돌갈.png", "keywords": ["진돌갈"] },
+  { "name": "진돌깡.png", "keywords": ["진돌깡"] },
+  { "name": "진돌따봉.png", "keywords": ["진돌따봉"] },
+  { "name": "진돌야스.png", "keywords": ["진돌야스"] },
+  { "name": "진돌살1자.png", "keywords": ["진돌살1자"] },
+  { "name": "진돌하트.png", "keywords": ["진돌하트"] },
+  { "name": "안아줘요.png", "keywords": ["안아줘요"] },
+
+
+  /*분류전 디시콘 리스트*/
+  { "name": "불편.png", "keywords": ["불편"] },
+  { "name": "편안.png", "keywords": ["편안"] },
+
+
+  /**
+   * 230121 수동 추가
+   * 커미션 등으로 저작권 문제 있을만한 것은
+   * 제외하였음.
+   * 
+   * 제외 목록: 연탄앵무, 진돌교주
+   */
+
+  { "name": "아오지.png", "keywords": ["아오지"] },
+  { "name": "진돌신.png", "keywords": ["진돌신"] },
+  { "name": "개못.png", "keywords": ["개못"] },
+  { "name": "파로마.gif", "keywords": ["파로마"] },
+  { "name": "페페덜덜덜.gif", "keywords": ["페페덜덜덜"] },
+  { "name": "두근두근.gif", "keywords": ["두근두근"] },
+  { "name": "페바.gif", "keywords": ["페바"] },
+  { "name": "진돌아.gif", "keywords": ["진돌아"] },
+  // { "name": "연탄앵무.gif", "keywords": ["연탄앵무"] },
+  { "name": "진돌병신.png", "keywords": ["진돌병신"] },
+  { "name": "꼬우면.gif", "keywords": ["꼬우면"] },
+
+  // 볼트콘 목록
+  { "name": "볼트기계.png", "keywords": ["볼트기계"] },
+  { "name": "볼트결혼.png", "keywords": ["볼트결혼"] },
+  { "name": "볼트비밀친구.png", "keywords": ["볼트비밀친구"] },
+  { "name": "볼트군인.png", "keywords": ["볼트군인"] },
+  { "name": "볼트닌자.png", "keywords": ["볼트닌자"] },
+
+  { "name": "볼트인육.png", "keywords": ["볼트인육"] },
+  { "name": "볼트핫산.png", "keywords": ["볼트핫산"] },
+  { "name": "볼트주사.png", "keywords": ["볼트주사"] },
+  { "name": "볼트빼액.png", "keywords": ["볼트빼액"] },
+  { "name": "볼트꺼져.png", "keywords": ["볼트꺼져"] },
+
+  { "name": "볼트닥쳐.png", "keywords": ["볼트닥쳐"] },
+  { "name": "볼트안마.png", "keywords": ["볼트안마"] },
+  { "name": "볼트망치.png", "keywords": ["볼트망치"] },
+  { "name": "볼트빠따.png", "keywords": ["볼트빠따"] },
+  { "name": "볼트수류탄.png", "keywords": ["볼트수류탄"] },
+
+  { "name": "볼트전기톱.png", "keywords": ["볼트전기톱"] },
+  { "name": "볼트장전.png", "keywords": ["볼트장전"] },
+  { "name": "볼트저격.png", "keywords": ["볼트저격"] },
+  { "name": "볼트라이플.png", "keywords": ["볼트라이플"] },
+  { "name": "볼트화염병.png", "keywords": ["볼트화염병"] },
+
+  { "name": "볼트주먹.png", "keywords": ["볼트주먹"] },
+  { "name": "볼트컴온.png", "keywords": ["볼트컴온"] },
+  { "name": "볼트투창.png", "keywords": ["볼트투창"] },
+  { "name": "볼트아킴보.png", "keywords": ["볼트아킴보"] },
+  { "name": "볼트천사.png", "keywords": ["볼트천사"] },
+
+  { "name": "볼트악마.png", "keywords": ["볼트악마"] },
+  { "name": "볼트고인.png", "keywords": ["볼트고인"] },
+  { "name": "볼트부활.png", "keywords": ["볼트부활"] },
+  { "name": "볼트띠용.png", "keywords": ["볼트띠용"] },
+  { "name": "볼트신문.png", "keywords": ["볼트신문"] },
+
+  { "name": "볼트눈물.png", "keywords": ["볼트눈물"] },
+  { "name": "볼트투시.png", "keywords": ["볼트투시"] },
+  { "name": "볼트토끼.png", "keywords": ["볼트토끼"] },
+  { "name": "볼트포기.png", "keywords": ["볼트포기"] },
+  { "name": "볼트자살.png", "keywords": ["볼트자살"] },
+
+  { "name": "볼트게이.png", "keywords": ["볼트게이"] },
+  { "name": "볼트딸딸.png", "keywords": ["볼트딸딸"] },
+  { "name": "볼트음주.png", "keywords": ["볼트음주"] },
+  { "name": "볼트수면.png", "keywords": ["볼트수면"] },
+  { "name": "볼트우승.png", "keywords": ["볼트우승"] },
+
+  { "name": "볼트루저.png", "keywords": ["볼트루저"] },
+  { "name": "볼트구토.png", "keywords": ["볼트구토"] },
+  { "name": "볼트예수.png", "keywords": ["볼트예수"] },
+  { "name": "볼트연설.png", "keywords": ["볼트연설"] },
+  { "name": "볼트공중.png", "keywords": ["볼트공중"] },
+
+  { "name": "볼트엄격.png", "keywords": ["볼트엄격"] },
+  { "name": "볼트진지.png", "keywords": ["볼트진지"] },
+  { "name": "볼트근엄.png", "keywords": ["볼트근엄"] },
+  { "name": "볼트펄럭.png", "keywords": ["볼트펄럭"] },
+  { "name": "볼트답답.png", "keywords": ["볼트답답"] },
+
+  { "name": "볼트절망.png", "keywords": ["볼트절망"] },
+  { "name": "볼트관전.png", "keywords": ["볼트관전"] },
+  { "name": "볼트통수.png", "keywords": ["볼트통수"] },
+  { "name": "볼트연주.png", "keywords": ["볼트연주"] },
+  { "name": "볼트파오후.png", "keywords": ["볼트파오후"] },
+
+  { "name": "볼트딴딴.png", "keywords": ["볼트딴딴"] },
+  { "name": "볼트처형.png", "keywords": ["볼트처형"] },
+  { "name": "볼트명상.png", "keywords": ["볼트명상"] },
+  { "name": "볼트마약.png", "keywords": ["볼트마약"] },
+  { "name": "볼트떠남.png", "keywords": ["볼트떠남"] },
+
+  { "name": "볼트경례.png", "keywords": ["볼트경례"] },
+  { "name": "볼트근육.png", "keywords": ["볼트근육"] },
+  { "name": "볼트자본.png", "keywords": ["볼트자본"] },
+  { "name": "볼트논리.png", "keywords": ["볼트논리"] },
+  { "name": "볼트따봉.png", "keywords": ["볼트따봉"] },
+
+  { "name": "볼트찡긋.png", "keywords": ["볼트찡긋"] },
+  { "name": "볼트불만.png", "keywords": ["볼트불만"] },
+  { "name": "볼트검.png", "keywords": ["볼트검"] },
+  { "name": "볼트기상.png", "keywords": ["볼트기상"] },
+  { "name": "볼트독서.png", "keywords": ["볼트독서"] },
+
+  { "name": "볼트전구.png", "keywords": ["볼트전구"] },
+  { "name": "볼트천재.png", "keywords": ["볼트천재"] },
 
 ];
